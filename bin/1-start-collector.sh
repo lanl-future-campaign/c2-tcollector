64db66ca92f1fa38c622bfd9547a22178cedff5d
Qing Zheng <qzheng@lanl.gov>
2022-10-03 18:57:05 -0600

    reduce -x scope

--- bin/1-start-collector.sh
+++ bin/1-start-collector.sh
@@ -1,6 +1,8 @@
-#!/bin/bash -xu
+#!/bin/bash -u
 
 dir0=$(cd `dirname $0` && pwd)
+
+set -x
 
 $dir0/../tcollector start --host=192.168.1.1 --t host=`hostname -I` --logfile=/tmp/tcollector.log --pidfile=/tmp/tcollector.pid
 
--- bin/2-stop-collector.sh
+++ bin/2-stop-collector.sh
@@ -1,6 +1,8 @@
-#!/bin/bash -xu
+#!/bin/bash -u
 
 dir0=$(cd `dirname $0` && pwd)
+
+set -x
 
 $dir0/../tcollector stop
 
